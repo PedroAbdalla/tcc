0275eca0d5e065e53a31213d643853350b5afe9e
PedroAbdalla <pedroabdalla@outlook.com>
2017-12-11 17:29:20 -0200

    correcao menu admin

--- view/restrito/admin.php
+++ view/restrito/admin.php
@@ -11,7 +11,7 @@ if(!empty($_SESSION['msg'])){ ?>
 	unset($_SESSION['tipo_msg']);
 } ?>
 <ul class="list-inline col-xs-12">
-	<li><a href="novo_usuario"><button type="button" class="btn btn-default">Novo Usuário</button></a></li>
-	<li><a href="lista_usuario"><button type="button" class="btn btn-default">Listar Usuários</button></a></li>
-	<li><a href="lista_tabela_padrao"><button type="button" class="btn btn-default">Tabela Padrão</button></a></li>
+	<li><a href="../../tcc/novo_usuario"><button type="button" class="btn btn-default">Novo Usuário</button></a></li>
+	<li><a href="../../tcc/lista_usuario"><button type="button" class="btn btn-default">Listar Usuários</button></a></li>
+	<li><a href="../../tcc/lista_tabela_padrao"><button type="button" class="btn btn-default">Tabela Padrão</button></a></li>
 </ul>
